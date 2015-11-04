8aa2527fbcbb45d3cb1261fcd5f84714286b80fb
sarah-story <sarah.f.lavendol@vanderbilt.edu>
2015-11-04 12:02:22 -0600

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+        // GET: /HelloWorld/Welcome/ 
+        public ActionResult Welcome(string name, int ID = 1)
+        {
+            ViewBag.Message = "Hi, " + name;
+            ViewBag.NumTimes = ID;
+            return View();
+        }
+    }
+}
